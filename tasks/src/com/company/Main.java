107c27a9f257a56781ea9f34544921abd32869f1
Bermet <bermetk.6789@gmail.com>
2021-04-27 00:02:46 +0600

    add task A

--- tasks/src/com/company/Main.java
+++ tasks/src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Введите количество студентов:");
+        int amount = scanner.nextInt();
+        Student[] studentList = new Student[amount];
+        for (int i = 0; i < studentList.length; i++) {
+            studentList[i] = new Student();
+            System.out.print("Введите ФИО студента: ");
+            if(i==0)
+            scanner.nextLine();
+            studentList[i].fullName = scanner.nextLine();
+            System.out.print("Введите группу студента: ");
+            studentList[i].group = scanner.nextLine();
+            System.out.print("Введите средний балл студента: ");
+            studentList[i].gpa = scanner.nextLine();
+            System.out.print("Введите дату рождения студента: ");
+            studentList[i].dateOfBirth = scanner.nextLine();
+            System.out.print("Введите адрес проживания студента: ");
+            studentList[i].address = scanner.nextLine();
+        }
+        for (int i = 0; i < amount; i++) {
+            System.out.printf(" ФИО: %s\n Группа: %s\n Средний балл: %s\n Дата рождения: %s\n Адрес : %s\n\n",studentList[i].fullName,studentList[i].group, studentList[i].gpa, studentList[i].dateOfBirth,studentList[i].address);
+        }
+
     }
 }
---
+++ tasks/src/com/company/Student.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Student {
+    String fullName;
+    String group;
+    String dateOfBirth;
+    String address;
+    String gpa;
+}
