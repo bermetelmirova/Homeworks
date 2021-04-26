2225ac139806998eaff1dcd56622a08da7eb0d17
Bermet <bermetk.6789@gmail.com>
2021-04-27 00:44:41 +0600

    add task B

---
+++ tasks/src/com/company/IntergalacticRace.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class IntergalacticRace {
+    String planet;
+    String raceName;
+    String officialLanguage;
+    int codeOfPlanet;
+    boolean isWater;
+    boolean isAir;
+}
--- tasks/src/com/company/Main.java
+++ tasks/src/com/company/Main.java
@@ -12,8 +12,8 @@ public class Main {
         for (int i = 0; i < studentList.length; i++) {
             studentList[i] = new Student();
             System.out.print("Введите ФИО студента: ");
-            if(i==0)
-            scanner.nextLine();
+            if (i == 0)
+                scanner.nextLine();
             studentList[i].fullName = scanner.nextLine();
             System.out.print("Введите группу студента: ");
             studentList[i].group = scanner.nextLine();
@@ -24,9 +24,32 @@ public class Main {
             System.out.print("Введите адрес проживания студента: ");
             studentList[i].address = scanner.nextLine();
         }
-        for (int i = 0; i < amount; i++) {
-            System.out.printf(" ФИО: %s\n Группа: %s\n Средний балл: %s\n Дата рождения: %s\n Адрес : %s\n\n",studentList[i].fullName,studentList[i].group, studentList[i].gpa, studentList[i].dateOfBirth,studentList[i].address);
+        for (int i = 0; i < studentList.length; i++) {
+            System.out.printf(" ФИО: %s\n Группа: %s\n Средний балл: %s\n Дата рождения: %s\n Адрес : %s\n\n", studentList[i].fullName, studentList[i].group, studentList[i].gpa, studentList[i].dateOfBirth, studentList[i].address);
         }
-
+        System.out.println("Введите количество рас:");
+        int amount2 = scanner.nextInt();
+        IntergalacticRace[] raceList = new IntergalacticRace[amount2];
+        for (int i = 0; i < raceList.length; i++) {
+            raceList[i] = new IntergalacticRace();
+            System.out.print("Введите название планеты: ");
+            if (i == 0)
+                scanner.nextLine();
+            raceList[i].planet = scanner.nextLine();
+            System.out.print("Введите код планеты: ");
+            raceList[i].codeOfPlanet = scanner.nextInt();
+            System.out.print("Есть ли воздух?: ");
+            raceList[i].isAir = scanner.nextBoolean();
+            System.out.print("Есть ли вода?: ");
+            raceList[i].isWater = scanner.nextBoolean();
+            System.out.print("Введите название расы планеты: ");
+            scanner.nextLine();
+            raceList[i].raceName = scanner.nextLine();
+            System.out.print("Введите официальный язык планеты: ");
+            raceList[i].officialLanguage = scanner.nextLine();
+        }
+        for (int i = 0; i < raceList.length; i++) {
+            System.out.printf(" Планета: %s\n Есть воздух: %s\n Есть вода: %s\n Раса: %s\n Официальный язык : %s\n\n",  raceList[i].planet,  raceList[i].isAir,  raceList[i].isWater,  raceList[i].raceName,  raceList[i].officialLanguage);
+        }
     }
 }
