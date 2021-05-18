d1cc303a2bfaf5eb771e40a1dd2527ea1c8b4c9e
Bermet <bermetk.6789@gmail.com>
2021-05-18 17:41:13 +0600

    task23 is added

--- tasks/src/com/company/Main.java
+++ tasks/src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        final Parabola[] parabolas = new Parabola[2];
+        parabolas[0] = new Parabola(10, true);
+        parabolas[1] = new Parabola(-10, false);
+
     }
 }
