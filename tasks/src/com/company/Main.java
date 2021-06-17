cbc8d7310a29b11d402debe7a0e6ba290daf3575
Bermet <bermetk.6789@gmail.com>
2021-06-17 19:23:58 +0600

    change to 100 iterations

--- tasks/src/com/company/Main.java
+++ tasks/src/com/company/Main.java
@@ -14,16 +14,16 @@ public class Main {
             catArrayList.add(new Cat(name + i, age + i));
             catLinkedList.add(new Cat(name + i, age + i));
         }
-        int index = 499999;
+
         long start = System.currentTimeMillis();
         for (int i = 0; i < 100; i++) {
-            catArrayList.add(index, new Cat(name + i, age + i));
+            catArrayList.add(catArrayList.size() / 2, new Cat(name + i, age + i));
         }
         long end = System.currentTimeMillis();
         System.out.println("Время добавления в середину ArrayList: " + (end - start));
         start = System.currentTimeMillis();
         for (int i = 0; i < 100; i++) {
-            catLinkedList.add(index, new Cat(name + i, age + i));
+            catLinkedList.add(catLinkedList.size() / 2, new Cat(name + i, age + i));
         }
         end = System.currentTimeMillis();
 
@@ -56,9 +56,13 @@ public class Main {
         }
         end = System.currentTimeMillis();
         System.out.println("Время добавления в конец LinkedList: " + (end - start));
-
+        int half = catArrayList.size() / 2;
         start = System.currentTimeMillis();
-            catArrayList.get(catArrayList.size() / 2);
+        for (int i = 0; i < 100; i++) {
+            catArrayList.get(half);
+            half++;
+        }
+
         end = System.currentTimeMillis();
         System.out.println("Время считвания с середины ArrayList: " + (end - start));
 
@@ -68,36 +72,50 @@ public class Main {
         System.out.println("Время считвания с середины LinkedList: " + (end - start));
 
         start = System.currentTimeMillis();
-        for (int i = catArrayList.size()-1; i >= 0; i--) {
-            catArrayList.get(i);
+        for (int i = 0; i < 100; i++) {
+            catLinkedList.get(half);
+            half++;
         }
         end = System.currentTimeMillis();
         System.out.println("Время считвания с конца ArrayList: " + (end - start));
-
+        int tail = catLinkedList.size() - 1;
         start = System.currentTimeMillis();
-        for (int i = catLinkedList.size()-1; i >= 0; i--) {
-            catLinkedList.get(i);
+        for (int i = 100; i >= 0; i--) {
+            catLinkedList.get(tail);
+            tail--;
         }
         end = System.currentTimeMillis();
         System.out.println("Время считвания с конца LinkedList: " + (end - start));
 
         start = System.currentTimeMillis();
-        catArrayList.remove(catArrayList.size() / 2);
+        for (int i = 100; i >= 0; i--) {
+            catArrayList.remove(half);
+            half--;
+        }
         end = System.currentTimeMillis();
         System.out.println("Время удаления с середины ArrayList: " + (end - start));
 
         start = System.currentTimeMillis();
-        catArrayList.remove(catArrayList.size() - 1);
+        for (int i = 100; i >= 0; i--) {
+            catArrayList.remove(tail);
+            tail--;
+        }
         end = System.currentTimeMillis();
         System.out.println("Время удаления с конца ArrayList: " + (end - start));
 
         start = System.currentTimeMillis();
-        catLinkedList.remove(catLinkedList.size() / 2);
+        for (int i = 100 - 1; i >= 0; i--) {
+            catLinkedList.remove(half);
+            half--;
+        }
         end = System.currentTimeMillis();
         System.out.println("Время удаления с середины LinkedList: " + (end - start));
 
         start = System.currentTimeMillis();
-        catLinkedList.remove(catLinkedList.size() - 1);
+        for (int i = 100; i >= 0; i--) {
+            catLinkedList.remove(tail);
+            tail--;
+        }
         end = System.currentTimeMillis();
         System.out.println("Время удаления с конца LinkedList: " + (end - start));
 
