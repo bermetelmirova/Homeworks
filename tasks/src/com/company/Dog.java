6cca1fb522b657b2feb50901d87024cfe05c42c2
Bermet <bermetk.6789@gmail.com>
2021-06-25 20:56:18 +0600

    add task A,B

---
+++ tasks/src/com/company/Dog.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Objects;
+
+public class Dog {
+    private String name;
+    private int age;
+
+    public Dog() {
+    }
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Меня зовут "+ name +", мне " + age + " лет\n";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return age == dog.age && name.equals(dog.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
--- tasks/src/com/company/Main.java
+++ tasks/src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Main {
+    static Random random = new Random();
 
     public static void main(String[] args) {
-	// write your code here
+        Set<Dog> dalmatians = new HashSet<>();
+        String name = "Далматинец";
+        for (int i = 0; i < 40; i++) {
+            dalmatians.add(new Dog(name + " #" + (i + 1), random.nextInt(10) + 1));
+        }
+        System.out.println(dalmatians);
+        System.out.println(dalmatians.size());
     }
 }
